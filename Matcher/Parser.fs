d23188fcc5efc122b9e21658800db4851237c760
zoren <ss@configit.com>
2013-10-21 22:22:19 +0200

    added prodlang parser

---
+++ Matcher/Parser.fs
@@ -0,0 +1,27 @@
+﻿namespace Matcher
+
+open FParsec
+open Matcher.ProdLang
+module Parser =
+
+    let wsc = spaces 
+
+    let isAsciiIdStart c = isAsciiLetter c || c = '_' || c = '?'
+
+    let isAsciiIdContinue c = isAsciiLetter c || isDigit c || c = '_' || c = '\''
+    
+    let id = identifier (IdentifierOptions(isAsciiIdStart = isAsciiIdStart, 
+                                           isAsciiIdContinue = isAsciiIdContinue)) .>> wsc
+    
+    let objVar = id .>> pstring "." .>>. id 
+
+    let value = pfloat .>> wsc |>> fun cs -> cs.ToString()
+
+    let equals = objVar .>> pstring "=" .>> wsc .>>. value |>> fun cs -> Eq cs
+
+    let comp = stringReturn "true" TRUE .>> wsc <|> equals
+
+    let prod = sepBy1 comp (pstring "and" .>> wsc) .>> pstring ">>" .>> wsc .>>. id
+
+    let prods : Parser<System,Unit> = wsc >>. many prod |>> fun prods -> {productions = prods}
+
---
+++ Matcher/ParserScript.fsx
@@ -0,0 +1,23 @@
+﻿#r @"bin\Debug\FParsecCS.dll"
+#r @"bin\Debug\FParsec.dll"
+
+open FParsec
+
+#load "ProdLang.fs"
+#load "Parser.fs"
+
+open Matcher.Parser
+
+let testParser p str =
+    match run p str with
+    | Success(result, _, _)   -> result
+//    | Failure ->
+
+let testProdsString = @"
+    O.X = 1 and O.Y = 2 and O.Z = 3 >> P1
+    O.X = 1 and O.Y = 2 and O.V = 4 and O.U = 5 >> P2
+    O.X = 1 and O.Y = 2 and O.V = 4 and O.Z = 3 >> P3
+    true >> P4
+"
+let p = run Matcher.Parser.prods testProdsString
+let parsedProds = testParser Matcher.Parser.prods testProdsString
