37ebeddc2834eec8236131739e5a882855582496
zoren <ss@configit.com>
2014-01-08 21:00:50 +0100

    added support for true, made custom types for production state

--- Matcher/MultiInstEvaluator.fs
+++ Matcher/MultiInstEvaluator.fs
@@ -11,30 +11,25 @@ module MultiInstEvaluator =
         system : System
         }
 
-    let getObjVars conds = Seq.toList <| (Seq.distinct <| Seq.map (fun(Eq((objVar,_),_)) -> objVar) conds)
+    let getObjVar = function | Eq((objVar,_),_) -> Some objVar | _ -> None
+
+    let getObjVars conds = Seq.toList <| (Seq.distinct <| Seq.choose getObjVar conds)
 
     let getObjMaps conds insts =
         let objVars = getObjVars conds
         let combs = Util.generateCombinations (Seq.length objVars) insts
         Seq.map (fun l -> List.zip objVars l) combs
-        
-    let evalCond objMap (env:Environment) (Eq((objVar,var),value)) =
-        let inst = Option.get <| Util.lookupOpt objMap objVar
-        (Util.lookupOpt env (inst, var)) = Some value
 
-    let evalConds conds env =
-        let insts = Seq.distinct <| Seq.map (fun ((instId,_),_) -> instId) env
-        let objMaps = getObjMaps conds insts
-        let evalCondMap objMap = Seq.fold (&&) true (Seq.map (fun c -> evalCond objMap env c) conds)
-        Seq.filter (fun objMap -> (evalCondMap objMap) ) objMaps
-
-    let tryGetEvalEnvCond objMap (env:Environment) (Eq((objVar,var),value)) =
-        match Util.lookupOpt objMap objVar with
-            Some inst ->
-                if Seq.exists ((=) ((inst, var),value)) env then
-                     Some (inst,var, value)
-                else None
-            | None -> None
+    let tryGetEvalEnvCond objMap (env:Environment) cond =
+        match cond with
+            (Eq((objVar,var),value)) ->
+                match Util.lookupOpt objMap objVar with
+                    Some inst ->
+                        if Seq.exists ((=) ((inst, var),value)) env then
+                             Some (inst,var, value)
+                        else None
+                    | None -> None
+            | TRUE -> None
 
     let tryGetEvalEnvConds conds env =
         let insts = Seq.distinct <| Seq.map (fun ((instId,_),_) -> instId) env
@@ -48,10 +43,9 @@ module MultiInstEvaluator =
                         | None -> None
         Seq.choose (tryGetEvalObjMapEnvConds conds) objMaps
 
-    let evalState { env = env; system = {productions = productions}} =
-        (Seq.map (fun (conds,prodName) -> (prodName,evalConds conds !env)) productions,
-         Seq.map (fun (conds,prodName) -> (prodName,tryGetEvalEnvConds conds !env)) productions)
-    let evalStateList { env = env; system = {productions = productions}} : (string * (int * string * string) list list) list =
+    type ProductionState = (int * string * string) list
+    type ProductionsState = (string * ProductionState list) list
+    let evalStateList { env = env; system = {productions = productions}} : ProductionsState =
         List.map (fun (conds,prodName) -> (prodName,Seq.toList <| tryGetEvalEnvConds conds !env)) productions
     let mkSystem prods = {productions = prods}
     
