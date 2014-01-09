7d876f9dd0f6d9722a2c8aa64f689af81ec56601
zoren <ss@configit.com>
2014-01-09 22:29:53 +0100

    added simple example from doorenboos as test

---
+++ MatcherTest/Simple.fsx
@@ -0,0 +1,15 @@
+﻿#r @"bin\\Debug\Matcher.dll"
+
+
+open Matcher.Runner
+
+open Matcher.SimpleBuilder
+#load "SimpleExample.fs"
+open MatcherTest.SimpleExample
+
+let (dummy,alphas) = buildSetParents example   
+
+let _ = act alphas w1
+let _ = act alphas w5
+let _ = act alphas w9
+
---
+++ MatcherTest/SimpleExample.fs
@@ -0,0 +1,38 @@
+﻿namespace MatcherTest
+
+module SimpleExample =
+    open Matcher.ReteData
+    open Matcher.SimpleBuilder
+
+    let mkWme f = { fields = f }
+
+    let example = [(Var "x", Val "on", Var "y");
+                    (Var "y", Val "left-of", Var "z");
+                    (Var "z", Val "color", Val "red")]
+                        
+    let B1 = "B1"
+    let B2 = "B2"
+    let B3 = "B3"
+    let B4 = "B4"
+
+    let on = "on"
+    let color = "color"
+    let leftOf = "left-of"
+
+    let red = "red"
+    let blue = "blue"
+    let table = "table"
+    
+    let w1 = mkWme (B1,on,B2)
+    let w2 = mkWme (B1,on,B3)
+    let w3 = mkWme (B1,color,red)
+
+    let w4 = mkWme (B2,on,table)
+    let w5 = mkWme (B2,leftOf,B3)
+    let w6 = mkWme (B2,color,blue)
+
+    let w7 = mkWme (B3,leftOf,B4)
+    let w8 = mkWme (B3,on,table)
+    let w9 = mkWme (B3,color,red)
+
+    
