4573ffc896fd7a5f32713553f134dbabb2a9c87b
Søren Sjørup <ss@configit.com>
2014-04-03 09:43:57 +0200

    added script for REPL testing of Dumper

---
+++ Matcher/DumperScript.fsx
@@ -0,0 +1,19 @@
+﻿#r @"bin\Debug\CoreLib.dll"
+
+#load "ReteData.fs"
+#load "Dumper.fs"
+
+open Matcher.ReteData
+open Matcher.Dumper
+
+let productionNode = (mkProd "P1")
+
+let graph = Matcher.Dumper.buildGraphviz (productionNode, [])
+
+let joinNode = mkJoin [] [productionNode]
+
+let joinGraph = Matcher.Dumper.buildGraphviz (joinNode, [])
+
+let betaDummy = mkBetaMemDummy [joinNode]
+
+let betaGraph = Matcher.Dumper.buildGraphviz (betaDummy, [])
