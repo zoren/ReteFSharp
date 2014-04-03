bd5d58b52412646d941b38d7fee89ee6e5ae4eef
Søren Sjørup <ss@configit.com>
2014-04-03 09:46:04 +0200

    added graph generation from parsed rules

--- ProdLang0/ParserScript.fsx
+++ ProdLang0/ParserScript.fsx
@@ -1,6 +1,7 @@
 ﻿#r @"bin\Debug\CoreLib.dll"
 #r @"bin\Debug\FParsecCS.dll"
 #r @"bin\Debug\FParsec.dll"
+#r @"bin\Debug\Matcher.dll"
 
 open FParsec
 
@@ -16,7 +17,7 @@ open ProdLang0.Printer
 let testParser p str =
     match run p str with
     | Success(result, _, _)   -> result
-//    | Failure ->
+    | Failure(message,_,_) -> raise (new System.Exception(message))
 
 let testProdsString = @"
     O.X = 1 and O.Y = 2 and O.Z = 3 >> P1
@@ -31,6 +32,12 @@ let p = run ProdLang0.Parser.prods testProdsString
 
 let parsedProds = testParser prods testProdsString
 
-
 let printedProds = printSystem parsedProds
 let _ = System.Console.WriteLine(printedProds)
+
+#load "ReteBuilder.fs"
+
+let (rootnode, alphaMems) = ProdLang0.ReteBuilder.buildReteFromSystem parsedProds
+let graph = Matcher.Dumper.buildGraphviz (rootnode, Seq.map (fun (_,mem) -> mem) alphaMems)
+
+let _  = Matcher.Dumper.dumpToFile graph
